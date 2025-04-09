aa0b6be9492f624e681467a86636dd2aae6abe06
dmurawski018 <dmurawski018@gmail.com>
2025-04-09 15:16:04 +0200

    django routes

--- django/hello/views.py
+++ django/hello/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
