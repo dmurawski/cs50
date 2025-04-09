51a89b4fc3cf1476fba6deeb412a476735b2281a
dmurawski018 <dmurawski018@gmail.com>
2025-04-09 19:41:35 +0200

    add redirect and another solution for forms and use django session

--- django/tasks/views.py
+++ django/tasks/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import HttpResponseRedirect, render
+from django.urls import reverse
 
 # Create your views here.
 
-tasks = ["foo", "bar", "baz"]
-
 
 def index(request):
-    context = {"tasks": tasks}
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    context = {"tasks": request.session["tasks"]}
     return render(request, "tasks/index.html", context=context)
 
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+
+        else:
+            return render(request, "tasks/add.html", {"form": form})
+
+    return render(request, "tasks/add.html", {"form": NewTaskForm()})
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
